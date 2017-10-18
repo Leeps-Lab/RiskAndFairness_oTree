23ca0aca22adda69f36ef03154430e4a63a51e51
Rachel Chen <me@rachelchen.me>
2017-10-17 18:41:37 -0700

    notes

--- config.py
+++ config.py
@@ -1,3 +1,7 @@
+'''
+Please use lowercase for dictionary keys in the future // Rachel
+'''
+
 data = [
     {'mode': 'probability', 'a_x': 80, 'a_y':0, 'b_x': 0, 'b_y': 80},
     {'mode': 'independent', 'm': 100, 'p_x': 2, 'p_y': 1, 'prob_a': 0.5},
