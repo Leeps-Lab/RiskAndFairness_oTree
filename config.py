08a523635df4655d9b089d9b4e8b67e55b99e0f6
Eli Pandolfo <epandolf@ucsc.edu>
2017-11-05 21:39:43 -0800

    checked that config had wrong format for fixedother

--- config.py
+++ config.py
@@ -13,54 +13,54 @@ Please use lowercase for dictionary keys in the future // Rachel
 import random
 
 data = [
-    [
-    {'mode': 'det_giv', 'm': 50, 'p_x': 0.50, 'p_y': 1, 'prob_a': 50, 'label': {'x': 'Your Tokens', 'y': 'Partner\'s Tokens'}},
-    {'mode': 'det_giv', 'm': 55, 'p_x': 0.75, 'p_y': 1, 'prob_a': 50, 'label': {'x': 'Your Tokens', 'y': 'Partner\'s Tokens'}},
-    {'mode': 'det_giv', 'm': 60, 'p_x': 1.00, 'p_y': 1, 'prob_a': 50, 'label': {'x': 'Your Tokens', 'y': 'Partner\'s Tokens'}},
-    {'mode': 'det_giv', 'm': 65, 'p_x': 1.25, 'p_y': 1, 'prob_a': 50, 'label': {'x': 'Your Tokens', 'y': 'Partner\'s Tokens'}},
-    {'mode': 'det_giv', 'm': 70, 'p_x': 1.50, 'p_y': 1, 'prob_a': 50, 'label': {'x': 'Your Tokens', 'y': 'Partner\'s Tokens'}},
-    {'mode': 'det_giv', 'm': 75, 'p_x': 1.75, 'p_y': 1, 'prob_a': 50, 'label': {'x': 'Your Tokens', 'y': 'Partner\'s Tokens'}},
-    {'mode': 'det_giv', 'm': 80, 'p_x': 2.00, 'p_y': 1, 'prob_a': 50, 'label': {'x': 'Your Tokens', 'y': 'Partner\'s Tokens'}},
-    {'mode': 'det_giv', 'm': 85, 'p_x': 2.25, 'p_y': 1, 'prob_a': 50, 'label': {'x': 'Your Tokens', 'y': 'Partner\'s Tokens'}},
-    {'mode': 'det_giv', 'm': 90, 'p_x': 2.50, 'p_y': 1, 'prob_a': 50, 'label': {'x': 'Your Tokens', 'y': 'Partner\'s Tokens'}},
-    {'mode': 'det_giv', 'm': 95, 'p_x': 2.75, 'p_y': 1, 'prob_a': 50, 'label': {'x': 'Your Tokens', 'y': 'Partner\'s Tokens'}}
-    ],
-    [
-    {'mode': 'sec_1bl_1ch'   , 'm': 50, 'p_x': 0.60, 'p_y': 1, 'prob_a': 50, 'label': {'x': 'State A (50%)','y': 'State B (50%)'}},
-    {'mode': 'sec_1bl_1ch'   , 'm': 52, 'p_x': 0.80, 'p_y': 1, 'prob_a': 50, 'label': {'x': 'State A (50%)','y': 'State B (50%)'}},
-    {'mode': 'sec_1bl_1ch'   , 'm': 54, 'p_x': 1.00, 'p_y': 1, 'prob_a': 50, 'label': {'x': 'State A (50%)','y': 'State B (50%)'}},
-    {'mode': 'sec_1bl_1ch'   , 'm': 56, 'p_x': 1.20, 'p_y': 1, 'prob_a': 50, 'label': {'x': 'State A (50%)','y': 'State B (50%)'}},
-    {'mode': 'sec_1bl_1ch'   , 'm': 58, 'p_x': 1.40, 'p_y': 1, 'prob_a': 50, 'label': {'x': 'State A (50%)','y': 'State B (50%)'}},
-    {'mode': 'sec_1bl_1ch'   , 'm': 60, 'p_x': 1.60, 'p_y': 1, 'prob_a': 50, 'label': {'x': 'State A (50%)','y': 'State B (50%)'}},
-    {'mode': 'sec_1bl_1ch'   , 'm': 62, 'p_x': 1.80, 'p_y': 1, 'prob_a': 50, 'label': {'x': 'State A (50%)','y': 'State B (50%)'}},
-    {'mode': 'sec_1bl_1ch'   , 'm': 64, 'p_x': 2.00, 'p_y': 1, 'prob_a': 50, 'label': {'x': 'State A (50%)','y': 'State B (50%)'}},
-    {'mode': 'sec_1bl_1ch'   , 'm': 66, 'p_x': 2.20, 'p_y': 1, 'prob_a': 50, 'label': {'x': 'State A (50%)','y': 'State B (50%)'}},
-    {'mode': 'sec_1bl_1ch'   , 'm': 68, 'p_x': 2.40, 'p_y': 1, 'prob_a': 50, 'label': {'x': 'State A (50%)','y': 'State B (50%)'}}
-    ],
-    [
-    {'mode': 'sec_2bl_1ch'   , 'm': 50, 'p_x': 0.60, 'p_y': 1, 'prob_a': 50, 'label': {'x': 'State A (50%)','y': 'State B (50%)'}},
-    {'mode': 'sec_2bl_1ch'   , 'm': 52, 'p_x': 0.80, 'p_y': 1, 'prob_a': 50, 'label': {'x': 'State A (50%)','y': 'State B (50%)'}},
-    {'mode': 'sec_2bl_1ch'   , 'm': 54, 'p_x': 1.00, 'p_y': 1, 'prob_a': 50, 'label': {'x': 'State A (50%)','y': 'State B (50%)'}},
-    {'mode': 'sec_2bl_1ch'   , 'm': 56, 'p_x': 1.20, 'p_y': 1, 'prob_a': 50, 'label': {'x': 'State A (50%)','y': 'State B (50%)'}},
-    {'mode': 'sec_2bl_1ch'   , 'm': 58, 'p_x': 1.40, 'p_y': 1, 'prob_a': 50, 'label': {'x': 'State A (50%)','y': 'State B (50%)'}},
-    {'mode': 'sec_2bl_1ch'   , 'm': 60, 'p_x': 1.60, 'p_y': 1, 'prob_a': 50, 'label': {'x': 'State A (50%)','y': 'State B (50%)'}},
-    {'mode': 'sec_2bl_1ch'   , 'm': 62, 'p_x': 1.80, 'p_y': 1, 'prob_a': 50, 'label': {'x': 'State A (50%)','y': 'State B (50%)'}},
-    {'mode': 'sec_2bl_1ch'   , 'm': 64, 'p_x': 2.00, 'p_y': 1, 'prob_a': 50, 'label': {'x': 'State A (50%)','y': 'State B (50%)'}},
-    {'mode': 'sec_2bl_1ch'   , 'm': 66, 'p_x': 2.20, 'p_y': 1, 'prob_a': 50, 'label': {'x': 'State A (50%)','y': 'State B (50%)'}},
-    {'mode': 'sec_2bl_1ch'   , 'm': 68, 'p_x': 2.40, 'p_y': 1, 'prob_a': 50, 'label': {'x': 'State A (50%)','y': 'State B (50%)'}}
-    ],
-    [
-    {'mode': 'sec_ownrisk'   , 'm': 50, 'p_x': 0.60, 'p_y': 1, 'prob_a': 50, 'label': {'x': 'State A (50%)','y': 'State B (50%)'}},
-    {'mode': 'sec_ownrisk'   , 'm': 52, 'p_x': 0.80, 'p_y': 1, 'prob_a': 50, 'label': {'x': 'State A (50%)','y': 'State B (50%)'}},
-    {'mode': 'sec_ownrisk'   , 'm': 54, 'p_x': 1.00, 'p_y': 1, 'prob_a': 50, 'label': {'x': 'State A (50%)','y': 'State B (50%)'}},
-    {'mode': 'sec_ownrisk'   , 'm': 56, 'p_x': 1.20, 'p_y': 1, 'prob_a': 50, 'label': {'x': 'State A (50%)','y': 'State B (50%)'}},
-    {'mode': 'sec_ownrisk'   , 'm': 58, 'p_x': 1.40, 'p_y': 1, 'prob_a': 50, 'label': {'x': 'State A (50%)','y': 'State B (50%)'}},
-    {'mode': 'sec_ownrisk'   , 'm': 60, 'p_x': 1.60, 'p_y': 1, 'prob_a': 50, 'label': {'x': 'State A (50%)','y': 'State B (50%)'}},
-    {'mode': 'sec_ownrisk'   , 'm': 62, 'p_x': 1.80, 'p_y': 1, 'prob_a': 50, 'label': {'x': 'State A (50%)','y': 'State B (50%)'}},
-    {'mode': 'sec_ownrisk'   , 'm': 64, 'p_x': 2.00, 'p_y': 1, 'prob_a': 50, 'label': {'x': 'State A (50%)','y': 'State B (50%)'}},
-    {'mode': 'sec_ownrisk'   , 'm': 66, 'p_x': 2.20, 'p_y': 1, 'prob_a': 50, 'label': {'x': 'State A (50%)','y': 'State B (50%)'}},
-    {'mode': 'sec_ownrisk'   , 'm': 68, 'p_x': 2.40, 'p_y': 1, 'prob_a': 50, 'label': {'x': 'State A (50%)','y': 'State B (50%)'}}
-    ],
+    # [
+    # {'mode': 'det_giv', 'm': 50, 'p_x': 0.50, 'p_y': 1, 'prob_a': 50, 'label': {'x': 'Your Tokens', 'y': 'Partner\'s Tokens'}},
+    # {'mode': 'det_giv', 'm': 55, 'p_x': 0.75, 'p_y': 1, 'prob_a': 50, 'label': {'x': 'Your Tokens', 'y': 'Partner\'s Tokens'}},
+    # {'mode': 'det_giv', 'm': 60, 'p_x': 1.00, 'p_y': 1, 'prob_a': 50, 'label': {'x': 'Your Tokens', 'y': 'Partner\'s Tokens'}},
+    # {'mode': 'det_giv', 'm': 65, 'p_x': 1.25, 'p_y': 1, 'prob_a': 50, 'label': {'x': 'Your Tokens', 'y': 'Partner\'s Tokens'}},
+    # {'mode': 'det_giv', 'm': 70, 'p_x': 1.50, 'p_y': 1, 'prob_a': 50, 'label': {'x': 'Your Tokens', 'y': 'Partner\'s Tokens'}},
+    # {'mode': 'det_giv', 'm': 75, 'p_x': 1.75, 'p_y': 1, 'prob_a': 50, 'label': {'x': 'Your Tokens', 'y': 'Partner\'s Tokens'}},
+    # {'mode': 'det_giv', 'm': 80, 'p_x': 2.00, 'p_y': 1, 'prob_a': 50, 'label': {'x': 'Your Tokens', 'y': 'Partner\'s Tokens'}},
+    # {'mode': 'det_giv', 'm': 85, 'p_x': 2.25, 'p_y': 1, 'prob_a': 50, 'label': {'x': 'Your Tokens', 'y': 'Partner\'s Tokens'}},
+    # {'mode': 'det_giv', 'm': 90, 'p_x': 2.50, 'p_y': 1, 'prob_a': 50, 'label': {'x': 'Your Tokens', 'y': 'Partner\'s Tokens'}},
+    # {'mode': 'det_giv', 'm': 95, 'p_x': 2.75, 'p_y': 1, 'prob_a': 50, 'label': {'x': 'Your Tokens', 'y': 'Partner\'s Tokens'}}
+    # ],
+    # [
+    # {'mode': 'sec_1bl_1ch'   , 'm': 50, 'p_x': 0.60, 'p_y': 1, 'prob_a': 50, 'label': {'x': 'State A (50%)','y': 'State B (50%)'}},
+    # {'mode': 'sec_1bl_1ch'   , 'm': 52, 'p_x': 0.80, 'p_y': 1, 'prob_a': 50, 'label': {'x': 'State A (50%)','y': 'State B (50%)'}},
+    # {'mode': 'sec_1bl_1ch'   , 'm': 54, 'p_x': 1.00, 'p_y': 1, 'prob_a': 50, 'label': {'x': 'State A (50%)','y': 'State B (50%)'}},
+    # {'mode': 'sec_1bl_1ch'   , 'm': 56, 'p_x': 1.20, 'p_y': 1, 'prob_a': 50, 'label': {'x': 'State A (50%)','y': 'State B (50%)'}},
+    # {'mode': 'sec_1bl_1ch'   , 'm': 58, 'p_x': 1.40, 'p_y': 1, 'prob_a': 50, 'label': {'x': 'State A (50%)','y': 'State B (50%)'}},
+    # {'mode': 'sec_1bl_1ch'   , 'm': 60, 'p_x': 1.60, 'p_y': 1, 'prob_a': 50, 'label': {'x': 'State A (50%)','y': 'State B (50%)'}},
+    # {'mode': 'sec_1bl_1ch'   , 'm': 62, 'p_x': 1.80, 'p_y': 1, 'prob_a': 50, 'label': {'x': 'State A (50%)','y': 'State B (50%)'}},
+    # {'mode': 'sec_1bl_1ch'   , 'm': 64, 'p_x': 2.00, 'p_y': 1, 'prob_a': 50, 'label': {'x': 'State A (50%)','y': 'State B (50%)'}},
+    # {'mode': 'sec_1bl_1ch'   , 'm': 66, 'p_x': 2.20, 'p_y': 1, 'prob_a': 50, 'label': {'x': 'State A (50%)','y': 'State B (50%)'}},
+    # {'mode': 'sec_1bl_1ch'   , 'm': 68, 'p_x': 2.40, 'p_y': 1, 'prob_a': 50, 'label': {'x': 'State A (50%)','y': 'State B (50%)'}}
+    # ],
+    # [
+    # {'mode': 'sec_2bl_1ch'   , 'm': 50, 'p_x': 0.60, 'p_y': 1, 'prob_a': 50, 'label': {'x': 'State A (50%)','y': 'State B (50%)'}},
+    # {'mode': 'sec_2bl_1ch'   , 'm': 52, 'p_x': 0.80, 'p_y': 1, 'prob_a': 50, 'label': {'x': 'State A (50%)','y': 'State B (50%)'}},
+    # {'mode': 'sec_2bl_1ch'   , 'm': 54, 'p_x': 1.00, 'p_y': 1, 'prob_a': 50, 'label': {'x': 'State A (50%)','y': 'State B (50%)'}},
+    # {'mode': 'sec_2bl_1ch'   , 'm': 56, 'p_x': 1.20, 'p_y': 1, 'prob_a': 50, 'label': {'x': 'State A (50%)','y': 'State B (50%)'}},
+    # {'mode': 'sec_2bl_1ch'   , 'm': 58, 'p_x': 1.40, 'p_y': 1, 'prob_a': 50, 'label': {'x': 'State A (50%)','y': 'State B (50%)'}},
+    # {'mode': 'sec_2bl_1ch'   , 'm': 60, 'p_x': 1.60, 'p_y': 1, 'prob_a': 50, 'label': {'x': 'State A (50%)','y': 'State B (50%)'}},
+    # {'mode': 'sec_2bl_1ch'   , 'm': 62, 'p_x': 1.80, 'p_y': 1, 'prob_a': 50, 'label': {'x': 'State A (50%)','y': 'State B (50%)'}},
+    # {'mode': 'sec_2bl_1ch'   , 'm': 64, 'p_x': 2.00, 'p_y': 1, 'prob_a': 50, 'label': {'x': 'State A (50%)','y': 'State B (50%)'}},
+    # {'mode': 'sec_2bl_1ch'   , 'm': 66, 'p_x': 2.20, 'p_y': 1, 'prob_a': 50, 'label': {'x': 'State A (50%)','y': 'State B (50%)'}},
+    # {'mode': 'sec_2bl_1ch'   , 'm': 68, 'p_x': 2.40, 'p_y': 1, 'prob_a': 50, 'label': {'x': 'State A (50%)','y': 'State B (50%)'}}
+    # ],
+    # [
+    # {'mode': 'sec_ownrisk'   , 'm': 50, 'p_x': 0.60, 'p_y': 1, 'prob_a': 50, 'label': {'x': 'State A (50%)','y': 'State B (50%)'}},
+    # {'mode': 'sec_ownrisk'   , 'm': 52, 'p_x': 0.80, 'p_y': 1, 'prob_a': 50, 'label': {'x': 'State A (50%)','y': 'State B (50%)'}},
+    # {'mode': 'sec_ownrisk'   , 'm': 54, 'p_x': 1.00, 'p_y': 1, 'prob_a': 50, 'label': {'x': 'State A (50%)','y': 'State B (50%)'}},
+    # {'mode': 'sec_ownrisk'   , 'm': 56, 'p_x': 1.20, 'p_y': 1, 'prob_a': 50, 'label': {'x': 'State A (50%)','y': 'State B (50%)'}},
+    # {'mode': 'sec_ownrisk'   , 'm': 58, 'p_x': 1.40, 'p_y': 1, 'prob_a': 50, 'label': {'x': 'State A (50%)','y': 'State B (50%)'}},
+    # {'mode': 'sec_ownrisk'   , 'm': 60, 'p_x': 1.60, 'p_y': 1, 'prob_a': 50, 'label': {'x': 'State A (50%)','y': 'State B (50%)'}},
+    # {'mode': 'sec_ownrisk'   , 'm': 62, 'p_x': 1.80, 'p_y': 1, 'prob_a': 50, 'label': {'x': 'State A (50%)','y': 'State B (50%)'}},
+    # {'mode': 'sec_ownrisk'   , 'm': 64, 'p_x': 2.00, 'p_y': 1, 'prob_a': 50, 'label': {'x': 'State A (50%)','y': 'State B (50%)'}},
+    # {'mode': 'sec_ownrisk'   , 'm': 66, 'p_x': 2.20, 'p_y': 1, 'prob_a': 50, 'label': {'x': 'State A (50%)','y': 'State B (50%)'}},
+    # {'mode': 'sec_ownrisk'   , 'm': 68, 'p_x': 2.40, 'p_y': 1, 'prob_a': 50, 'label': {'x': 'State A (50%)','y': 'State B (50%)'}}
+    # ],
     [
     {'mode': 'sec_ownrisk_fixedother'   , 'm': 50, 'p_x': 0.60, 'p_y': 1, 'prob_a': 50, 'label': {'x': 'State A (50%)','y': 'State B (50%)'}},
     {'mode': 'sec_ownrisk_fixedother'   , 'm': 52, 'p_x': 0.80, 'p_y': 1, 'prob_a': 50, 'label': {'x': 'State A (50%)','y': 'State B (50%)'}},
