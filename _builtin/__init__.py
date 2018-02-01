f7227297a6476ee3b6a5df2241cd699fda7591fa
Eli Pandolfo <epandolf@ucsc.edu>
2018-01-31 23:19:45 -0800

    commit after fixing sneaky conflict in init.py

--- _builtin/__init__.py
+++ _builtin/__init__.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # This file is auto-generated.
 # It's used to aid autocompletion in code editors.
 
@@ -24,30 +23,3 @@ class Bot(otree.api.Bot):
         self.subsession = models.Subsession()
         self.group = models.Group()
         self.player = models.Player()
-=======
-# This file is auto-generated.
-# It's used to aid autocompletion in code editors.
-
-import otree.api
-from .. import models
-
-
-class Page(otree.api.Page):
-    def z_autocomplete(self):
-        self.subsession = models.Subsession()
-        self.group = models.Group()
-        self.player = models.Player()
-
-
-class WaitPage(otree.api.WaitPage):
-    def z_autocomplete(self):
-        self.subsession = models.Subsession()
-        self.group = models.Group()
-
-
-class Bot(otree.api.Bot):
-    def z_autocomplete(self):
-        self.subsession = models.Subsession()
-        self.group = models.Group()
-        self.player = models.Player()
->>>>>>> f554b66f3e5a525da88a0b678017652dcd325827
